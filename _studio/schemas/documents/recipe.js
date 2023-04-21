2e095982df536590b1e932384dd1baf270d70da8
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-04-21 22:35:05 +0200

    Created schema for recipe

---
+++ _studio/schemas/documents/recipe.js
@@ -0,0 +1,20 @@
+export default {
+	title: 'Cooking recipes',
+	name: 'cooking-recipes',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string'
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: 'name'
+			}
+		}
+	]
+}
