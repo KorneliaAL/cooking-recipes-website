8852c409b263ac0d9e36c38a7f87909277022e12
Kornelia Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-04-26 13:49:14 +0200

    Created Sanity database for cooking recipes, using schemas
    * Created description and image fields
    
    * Created field for ingredients
    
    * Created field for instructions
    
    * Created number field for cooking minutes
    
    * Created fields for diets
    
    * Added calories field
    
    * Created reference fields to category and cousine
    
    * Created schema for category
    
    * Created schema for cuisine
    
    * Imported schemas
    
    * Added servings field & set preview for ingredients
    
    * Rewritten ingredients field
    
    * Added alternative text and credit field to images
    
    * Formatted code

--- _studio/schemas/documents/recipe.js
+++ _studio/schemas/documents/recipe.js
@@ -1,6 +1,6 @@
 export default {
 	title: 'Cooking recipes',
-	name: 'cooking-recipes',
+	name: 'cookingRecipes',
 	type: 'document',
 	fields: [
 		{
@@ -15,6 +15,232 @@ export default {
 			options: {
 				source: 'name'
 			}
+		},
+		{
+			title: 'Description',
+			name: 'description',
+			type: 'text'
+		},
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'object',
+			fields: [
+				{
+					title: 'Image',
+					name: 'image',
+					type: 'image'
+				},
+				{
+					title: 'Alternative text',
+					name: 'altText',
+					type: 'string'
+				},
+				{
+					title: 'Credit',
+					name: 'credit',
+					type: 'url'
+				}
+			]
+		},
+		{
+			title: 'Servings',
+			name: 'servings',
+			type: 'number'
+		},
+		{
+			title: 'Ingredients List',
+			name: 'ingredientList',
+			type: 'array',
+			of: [
+				{
+					type: 'object',
+					fields: [
+						{
+							title: 'Name',
+							name: 'name',
+							type: 'string'
+						},
+						{
+							title: 'Ingredients',
+							name: 'ingredients',
+							type: 'array',
+							of: [
+								{
+									type: 'object',
+									fields: [
+										{
+											title: 'Name',
+											name: 'name',
+											type: 'string'
+										},
+										{
+											title: 'Amount',
+											name: 'amount',
+											type: 'number'
+										},
+										{
+											title: 'Unit',
+											name: 'unit',
+											type: 'string'
+										},
+										{
+											title: 'Measures',
+											name: 'measures',
+											type: 'object',
+											fields: [
+												{
+													title: 'Metric',
+													name: 'metric',
+													type: 'object',
+													fields: [
+														{
+															title: 'Amount',
+															name: 'amount',
+															type: 'number'
+														},
+														{
+															title: 'Unit Long',
+															name: 'unitLong',
+															type: 'string'
+														},
+														{
+															title: 'Unit Short',
+															name: 'unitShort',
+															type: 'string'
+														}
+													]
+												},
+												{
+													title: 'Us',
+													name: 'us',
+													type: 'object',
+													fields: [
+														{
+															title: 'Amount',
+															name: 'amount',
+															type: 'number'
+														},
+														{
+															title: 'Unit Long',
+															name: 'unitLong',
+															type: 'string'
+														},
+														{
+															title: 'Unit Short',
+															name: 'unitShort',
+															type: 'string'
+														}
+													]
+												}
+											]
+										}
+									],
+									preview: {
+										select: {
+											name: 'name',
+											amount: 'amount',
+											unit: 'unit'
+										},
+										prepare: (fields) => {
+											return {
+												title: `${fields.name}`,
+												subtitle: `${fields.amount} ${fields.unit}`
+											}
+										}
+									}
+								}
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Instructions',
+			name: 'instructions',
+			type: 'array',
+			of: [
+				{
+					type: 'object',
+					fields: [
+						{
+							title: 'Number',
+							name: 'number',
+							type: 'number'
+						},
+						{
+							title: 'Step',
+							name: 'step',
+							type: 'text'
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Ready in minutes',
+			name: 'readyInMinutes',
+			type: 'number'
+		},
+		{
+			title: 'Vegetarian',
+			name: 'vegetarian',
+			type: 'boolean'
+		},
+		{
+			title: 'Vegan',
+			name: 'vegan',
+			type: 'boolean',
+		},
+		{
+			title: 'Gluten free',
+			name: 'glutenFree',
+			type: 'boolean'
+		},
+		{
+			title: 'Dairy Free',
+			name: 'dairyFree',
+			type: 'boolean'
+		},
+		{
+			title: 'Calories',
+			name: 'calories',
+			type: 'object',
+			fields: [
+				{
+					title: 'Amount',
+					name: 'amount',
+					type: 'number'
+				},
+				{
+					title: 'Unit',
+					name: 'unit',
+					type: 'string'
+				}
+			]
+		},
+		{
+			title: 'Category',
+			name: 'category',
+			type: 'reference',
+			to: {
+				type: 'category'
+			}
+		},
+		{
+			title: 'Cuisines',
+			name: 'cuisines',
+			type: 'array',
+			of: [
+				{
+					type: 'reference',
+					to: {
+						type: 'cuisine'
+					}
+				}
+			]
 		}
 	]
 }
+
