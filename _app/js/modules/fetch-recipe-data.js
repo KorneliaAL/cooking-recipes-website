0ab6442bb5294191c680cc1106cfd7311c357d89
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-03 13:34:36 +0200

    Fetched data

---
+++ _app/js/modules/fetch-recipe-data.js
@@ -0,0 +1,33 @@
+import { sanity } from '../sanity.js';
+
+export default async function FetchRecipeData() {
+	const query = `* [_type == 'cookingRecipes'] {
+  'slug': slug.current,
+  'recipeName': name,
+  'ingredients': ingredientList[],
+  'intructions': instructions[],
+   'category': category-> {
+        name,
+        'categorySlug': slug.current,
+        'id': _id
+	},
+    'cookTime': readyInMinutes,
+    'cuisines': cuisines[] -> {
+        name,
+        'id': _id,
+        'slug': slug.current
+    },
+    servings,
+    'image': image{
+    'altText': altText,
+    'imageURL': image.asset -> .url
+  },
+  calories
+  }`;
+
+  const params = {};
+
+  const recipeData = await sanity.fetch(query, params);
+
+  return recipeData;
+}
---
+++ _app/js/sanity.js
@@ -0,0 +1,7 @@
+import { SanityClient } from "./util/sanity-client.js";
+
+export const sanity = SanityClient({
+	id: 'zih0t3ed',
+	dataset: 'production',
+	version: '2023-05-01'
+});
