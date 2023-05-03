700eca10f6df43c14b3807373b490cf746c139b8
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-03 20:57:24 +0200

    Created recipe link element

--- _app/js/modules/recipes-list.js
+++ _app/js/modules/recipes-list.js
@@ -1,2 +1,32 @@
 export default function RenderRecipes(recipes) {
+	const recipesContainer = document.querySelector('.recipes__list');
+
+	console.log(recipes[0].id)
+	if (recipesContainer) {
+		renderHTML();
+	}
+
+	function returnRecipeDOMElement(recipe) {
+		const recipeSlug = recipe.slug;
+		const recipeID = recipe.id;
+		const recipeURL = `/_app/recipe-description/inedx.html?recipe=${recipeSlug}`;
+
+		const recipeCardElement = document.createElement('a');
+
+		recipeCardElement.setAttribute('href', recipeURL);
+
+		recipeCardElement.classList.add('recipes__list-element', 'grid__column-mobile--12', 'grid__column--3');
+		
+		recipeCardElement.dataset.id = recipeID;
+
+		return recipeCardElement;		
+	}
+
+	function renderHTML() {
+		recipes.forEach(recipe => {
+			const recipeElement = returnRecipeDOMElement(recipe);
+
+			recipesContainer.appendChild(recipeElement)
+		})
+	}
 }
