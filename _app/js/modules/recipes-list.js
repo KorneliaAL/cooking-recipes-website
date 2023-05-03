5b2a846ff156352d30b2658c93c55d06488ec35b
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-03 21:41:42 +0200

    Created title and cook time elements

--- _app/js/modules/recipes-list.js
+++ _app/js/modules/recipes-list.js
@@ -1,7 +1,7 @@
 export default function RenderRecipes(recipes) {
 	const recipesContainer = document.querySelector('.recipes__list');
 
-	console.log(recipes[0].id)
+	console.log(recipes[0].cookTime)
 	if (recipesContainer) {
 		renderHTML();
 	}
@@ -11,23 +11,47 @@ export default function RenderRecipes(recipes) {
 		const recipeID = recipe.id;
 		const recipeURL = `/_app/recipe-description/inedx.html?recipe=${recipeSlug}`;
 		const recipeImageURL = recipe.image.imageURL;
+		const recipeName = recipe.recipeName;
+		const cookTime = recipe.cookTime;
 
 		const recipeCardElement = document.createElement('a');
 		const recipeImageElement = document.createElement('div');
 		const recipeFavoriteButtonElement = document.createElement('button');
+		const recipeInformationContainerElement = document.createElement('div');
+		const recipeInformationTitleElement = document.createElement('h3');
+		const recipeInformationTimeContainerElement = document.createElement('div');
+		const recipeInformationTimeIconElement = document.createElement('div');
+		const recipeInformationTimeElement = document.createElement('div');
 
 		recipeCardElement.setAttribute('href', recipeURL);
 		recipeImageElement.style.backgroundImage = `url('${recipeImageURL}')`;
+		recipeInformationTitleElement.textContent = recipeName;
+		recipeInformationTimeElement.textContent = `${cookTime} min`;
 
 		recipeCardElement.classList.add('recipes__list-element', 'grid__column-mobile--12', 'grid__column--3');
 		recipeImageElement.classList.add('recipes__list-element-image');
 		recipeFavoriteButtonElement.classList.add('recipes__list-element-favorite-button');
-		
+		recipeInformationContainerElement.classList.add('recipes__list-element-information');
+		recipeInformationTitleElement.classList.add('recipes__information-title');
+		recipeInformationTimeContainerElement.classList.add('recipes__information-time');
+		recipeInformationTimeIconElement.classList.add('recipes__information-time-icon');
+
 		recipeCardElement.dataset.id = recipeID;
 		recipeFavoriteButtonElement.dataset.id = recipeID;
 
 		recipeImageElement.appendChild(recipeFavoriteButtonElement);
-		recipeCardElement.append(recipeImageElement);
+		recipeInformationTimeContainerElement.append(
+			recipeInformationTimeIconElement,
+			recipeInformationTimeElement
+		)
+		recipeInformationContainerElement.append(
+			recipeInformationTitleElement, 
+			recipeInformationTimeContainerElement);
+
+		recipeCardElement.append(
+			recipeImageElement, 
+			recipeInformationContainerElement);
+
 		return recipeCardElement;		
 	}
 
