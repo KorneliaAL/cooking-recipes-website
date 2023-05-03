c51a313f71cacd88917306321a084c59c7ad6d7e
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-03 21:09:07 +0200

    Created image element & added favorite button

--- _app/js/modules/recipes-list.js
+++ _app/js/modules/recipes-list.js
@@ -10,15 +10,24 @@ export default function RenderRecipes(recipes) {
 		const recipeSlug = recipe.slug;
 		const recipeID = recipe.id;
 		const recipeURL = `/_app/recipe-description/inedx.html?recipe=${recipeSlug}`;
+		const recipeImageURL = recipe.image.imageURL;
 
 		const recipeCardElement = document.createElement('a');
+		const recipeImageElement = document.createElement('div');
+		const recipeFavoriteButtonElement = document.createElement('button');
 
 		recipeCardElement.setAttribute('href', recipeURL);
+		recipeImageElement.style.backgroundImage = `url('${recipeImageURL}')`;
 
 		recipeCardElement.classList.add('recipes__list-element', 'grid__column-mobile--12', 'grid__column--3');
+		recipeImageElement.classList.add('recipes__list-element-image');
+		recipeFavoriteButtonElement.classList.add('recipes__list-element-favorite-button');
 		
 		recipeCardElement.dataset.id = recipeID;
+		recipeFavoriteButtonElement.dataset.id = recipeID;
 
+		recipeImageElement.appendChild(recipeFavoriteButtonElement);
+		recipeCardElement.append(recipeImageElement);
 		return recipeCardElement;		
 	}
 
