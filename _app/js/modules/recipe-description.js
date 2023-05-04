b0e87e6944647a5d51953f448d293badc630b424
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-04 11:04:13 +0200

    Find current recipe based on URL query parameter

--- _app/js/modules/recipe-description.js
+++ _app/js/modules/recipe-description.js
@@ -1,3 +1,24 @@
 export default function RenderRecipeDescription(recipes) {
-	console.log(recipes);
+
+	const recipeDescription = document.querySelector('.description');
+	if (recipeDescription) {
+		const urlParams = new URLSearchParams(window.location.search);
+		const slug = urlParams.get('recipe');
+		
+		renderHTML(slug);
+	}
+
+	function returnRecipeInfoToDOMElement(recipe) {
+		
+	}
+
+	function renderHTML(slug) {
+		const currentRecipeDescription = recipes.find(recipe => recipe.slug === slug);
+
+		const recipeDescriptonInformation = returnRecipeInfoToDOMElement(currentRecipeDescription);
+
+		recipeDescription.append(recipeDescriptonInformation);
+	}
 }
+
+
