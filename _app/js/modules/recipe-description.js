eb04c437cda02b107c3b5d25092995435ed3e97c
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-05 20:29:13 +0200

    Added comments

--- _app/js/modules/recipe-description.js
+++ _app/js/modules/recipe-description.js
@@ -1,7 +1,14 @@
+/**
+Renders the recipe description on the webpage
+@param {Object} recipes - An object containing the recipe information to be displayed
+@returns {HTMLElement} - The recipe description elements to be appended to the DOM
+*/
+
 export default function RenderRecipeDescription(recipes) {
 
 	const recipeDescription = document.querySelector('.description');
 
+	 // Get the slug value from the URL parameters
 	if (recipeDescription) {
 		const urlParams = new URLSearchParams(window.location.search);
 		const slug = urlParams.get('recipe');
@@ -9,7 +16,16 @@ export default function RenderRecipeDescription(recipes) {
 		renderHTML(slug);
 	}
 
+
+	/**
+	Returns the recipe information as DOM elements
+	@param {object} recipe - The recipe data object
+	@returns {HTMLElement} - The recipe information DOM elements
+	*/
+
 	function returnRecipeInfoToDOMElement(recipe) {
+
+		// Extract the recipe data
 		const descriptionTitle = recipe.recipeName;
 		const caloryAmount = recipe.calories.amount;
 		const caloryUnit = recipe.calories.unit;
@@ -19,30 +35,27 @@ export default function RenderRecipeDescription(recipes) {
 		const timeText = 'min';
 		const favoriteButtonText = 'save to favorites';
 
+		// Creates the DOM elements for the recipe information
 		const descriptionInformationElement = document.createElement('div');
-
 		const descriptionTitleElement = document.createElement('h2');
-
 		const descriptionInformationFactsElement = document.createElement('div');
 		const descriptionFactCaloryElement = document.createElement('div');
 		const descriptionCaloryIconElement = document.createElement('div');
 		const descriptionCaloryAmountElement = document.createElement('div');
 		const descriptionCaloryUnitElement = document.createElement('div');
-
 		const descriptionFactServingsElement = document.createElement('div');
 		const descriptionServingsIconElement = document.createElement('div');
 		const descriptionServingsAmountElement = document.createElement('div');
 		const descriptionServingsTextElement = document.createElement('div');
-
 		const descriptionFactTimeElement = document.createElement('div');
 		const descriptionTimeIconElement = document.createElement('div');
 		const descriptionTimeAmountElement = document.createElement('div');
 		const descriptionTimeTextElement = document.createElement('div');
-
 		const descriptionFavoriteButtonElement = document.createElement('button');
 		const descriptionFavoriteButtonIconElement = document.createElement('div');
 		const descriptionFavoriteButtonTextElement = document.createElement('div');
 
+		// Set the text content for the recipe information elements
 		descriptionTitleElement.textContent = descriptionTitle;
 		descriptionCaloryAmountElement.textContent = caloryAmount;
 		descriptionCaloryUnitElement.textContent = caloryUnit;
@@ -51,12 +64,14 @@ export default function RenderRecipeDescription(recipes) {
 		descriptionFavoriteButtonTextElement.textContent = favoriteButtonText;
 		descriptionTimeAmountElement.textContent = timeAmount;
 
+		// Set the text content for servings amount, adding 0 if it's less than 10
 		if(servingsAmount < 10) {
 			descriptionServingsAmountElement.textContent = `0${servingsAmount}`;
 		} else {
 			descriptionServingsAmountElement.textContent = servingsAmount;
 		}
 
+		// Adds CSS classes to the recipe information elements
 		descriptionInformationElement.classList.add(
 			'description__information', 
 			'grid__column-start-desktop--2', 
@@ -121,6 +136,12 @@ export default function RenderRecipeDescription(recipes) {
 		return descriptionInformationElement;
 	}
 
+	/**
+	Returns a DOM element containing an image of the recipe
+	@param {Object} recipe - The recipe object containing the image data
+	@return {HTMLElement} - A DOM element containing the image of the recipe
+	*/
+
 	function returnRecipeImageToDOMElement(recipe) {
 		const imageUrl = recipe.image.imageURL;
 		const imageAltText = recipe.image.altText;
@@ -156,6 +177,12 @@ export default function RenderRecipeDescription(recipes) {
 
 		return descriptionIngredients;
 	}
+
+	/**
+	Returns a DOM element that contains the ingredients of a recipe.
+	@param {Object} ingredient - The ingredient object containing the information about the ingredients of the recipe.
+	@return {HTMLElement} An HTML element containing the ingredients of a recipe.
+	*/
 
 	function returnRecipeIngredientsToDOMElement(ingredient) {
 		const ingredientsContainer = document.createElement('div');
@@ -226,11 +253,20 @@ export default function RenderRecipeDescription(recipes) {
 		return descriptionInstruction;
 	}
 
-	function returnRecipeInstructionToDOMElement(instructions) {
+	/**
+	Returns a DOM element containing the instructions of a recipe.
+	@param {Object} recipe - An object containing the information of the current recipe.
+	@param {Array} recipe.instructions - An array of instructions of a recipe.
+	@param {string} recipe.instructions.step - A string containing the instruction text of a recipe.
+	
+	@returns {HTMLUListElement} A DOM element containing the instructions of a recipe.
+	*/
+
+	function returnRecipeInstructionToDOMElement(recipe) {
 		const instructionsListContainer = document.createElement('ul');
 
 		instructionsListContainer.classList.add('description__instruction-list')
-		instructions.instructions.forEach(instruction => {
+		recipe.instructions.forEach(instruction => {
 
 			const instructionListItemElement = document.createElement('li');
 			const instructionButton = document.createElement('button');
@@ -254,6 +290,13 @@ export default function RenderRecipeDescription(recipes) {
 		});
 		return instructionsListContainer;
 	}
+
+
+	/**
+	Renders the HTML for a given recipe slug.
+	@param {string} slug - The slug of the recipe to render.
+	@returns {void}
+	*/
 
 	function renderHTML(slug) {
 		const currentRecipeDescription = recipes.find(recipe => recipe.slug === slug);
