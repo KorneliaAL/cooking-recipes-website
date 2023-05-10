65648dd08cfb9fdfd41ca185e4e46cc209527fc0
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-10 11:56:16 +0200

    Added comments

--- _app/js/modules/favorite-button.js
+++ _app/js/modules/favorite-button.js
@@ -1,14 +1,28 @@
+/**
+ * Represents a favorite button.
+ * @param {Event} event - The event that triggered the button click.
+ */
+
 export default function FavoriteButton(event) {
 	event.preventDefault();
 
 	let favoritesRecipe = getFavoriteRecipeLocally();
 	const currentButton = event.target;
 
+	/**
+	 * Handles the click event on the favorite button.
+	 */
+
 	function handleFavoriteButtonClick() {
 		const id = currentButton.dataset.id;
 		toggleFavoriteRecipe(id);
 		renderHTML();
 	}
+
+	/**
+	 * Toggles the favorite status of a recipe.
+	 * @param {string} id - The ID of the recipe.
+	 */
 
 	function toggleFavoriteRecipe(id) {
 		if (favoritesRecipe.includes(id)) {
@@ -22,6 +36,11 @@ export default function FavoriteButton(event) {
 		storeFavoriteRecipeLocally();
 	}
 
+	/**
+	 * Retrieves the favorite recipes stored locally.
+	 * @returns {Array<string>} - The array of favorite recipe IDs.
+	 */
+
 	function getFavoriteRecipeLocally() {
 		const key = 'favorite-recipes';
 		const favoriteRecipeAsString = window.localStorage.getItem(key);
@@ -33,11 +52,19 @@ export default function FavoriteButton(event) {
 		}
 	}
 
+	/**
+	 * Stores the favorite recipes locally.
+	 */
+
 	function storeFavoriteRecipeLocally() {
 		const value = JSON.stringify(favoritesRecipe);
 		const key = 'favorite-recipes';
 		window.localStorage.setItem(key, value);
 	}
+
+	/**
+	 * Renders the HTML to reflect the updated favorite status.
+	 */
 
 	function renderHTML() {
 		currentButton.classList.toggle('recipes__list-element-favorite-button--active');
