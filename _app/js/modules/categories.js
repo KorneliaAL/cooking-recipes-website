fdd2ca94791f3e8d5a40b6bfc55c275a67bd126a
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-11 13:56:12 +0200

    Created category elements

--- _app/js/modules/categories.js
+++ _app/js/modules/categories.js
@@ -1,14 +1,11 @@
 export default function Categories(recipes) {
-	const categoryContainer = document.querySelector('.category');
-
-	
+	const categoryContainer = document.querySelector('.category__navigation-list');
 /**
  * @Todo - fetch category image in fetch recipe
  */
 	if (categoryContainer) {
 		createCategoryButtons();
 	}
-
 
 	function createCategoryButtons() {
 		const categories = [];
@@ -21,16 +18,48 @@ export default function Categories(recipes) {
 				categories.push(category);
 			}
 		});
-		console.log(categories)
 		renderHTML(categories);
+		// console.log(categories)
+	}
+
+	function returnCategoryDOMElement(category) {
+		const categoryName = category.name;
+		const imageURL = category.image.imageURL;
+		const altText = category.image.altText;
+		const id = category.id;
+
+		const listItemElement = document.createElement('li');
+		const buttonElement = document.createElement('button');
+		const imageElement = document.createElement('img');
+		const buttonTextElement = document.createElement('div');
+
+		imageElement.setAttribute('src', imageURL);
+		imageElement.setAttribute('alt', altText);
+
+		buttonElement.dataset.id = id;
+		buttonTextElement.textContent = categoryName;
+
+		listItemElement.classList.add('category__navigation-element');
+		buttonElement.classList.add('category__navigation-category-button')
+		imageElement.classList.add('category__navigation-element-image');
+		buttonTextElement.classList.add('category__navigation-element-title');
+
+		buttonElement.append(
+			imageElement,
+			buttonTextElement
+		);
+		listItemElement.appendChild(buttonElement);
+
+		return listItemElement;
 	}
 
 	function renderHTML(categories) {
 		categories.forEach(category => {
-			const buttonElement = document.createElement('button');
-			buttonElement.innerText = category.name;
-			categoryContainer.appendChild(buttonElement);
+			const categoryButtonElement = returnCategoryDOMElement(category);
+			categoryContainer.appendChild(categoryButtonElement);
+			returnCategoryDOMElement(category)
 		});
+		// console.log(categories);
 	}
 }
 
