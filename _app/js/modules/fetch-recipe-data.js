7669d15c5d9f7f8907583b7e9114aae994874fca
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-16 10:39:50 +0200

    Created temporary try/catch

--- _app/js/modules/fetch-recipe-data.js
+++ _app/js/modules/fetch-recipe-data.js
@@ -1,39 +1,46 @@
 import { sanity } from '../sanity.js';
 
 export default async function FetchRecipeData() {
-	const query = 
-		`* [_type == 'cookingRecipes'] {
-			'slug': slug.current,
-			'id': _id,
-			'recipeName': name,
-			'ingredients': ingredientList[],
-			'instructions': instructions[],
-			'category': category-> {
-				name,
-				'categorySlug': slug.current,
-				'id': _id,
-				'image': image {
-				'imageURL': image.asset -> url,
-				'altText': altText
-				}
-			},
-			'cookTime': readyInMinutes,
-			'cuisines': cuisines[] -> {
-				name,
-				'id': _id,
-				'slug': slug.current
-			},
-			servings,
-			'image': image{
-				'altText': altText,
-				'imageURL': image.asset -> .url
-			},
-			calories
-		}`;
+	try {
+		const query = `
+      *[ _type == 'cookingRecipes' ] {
+        'slug': slug.current,
+        'id': _id,
+        'recipeName': name,
+        'ingredients': ingredientList[],
+        'instructions': instructions[],
+        'category': category-> {
+          name,
+          'categorySlug': slug.current,
+          'id': _id,
+          'image': image {
+            'imageURL': image.asset->url,
+            'altText': altText
+          }
+        },
+        'cookTime': readyInMinutes,
+        'cuisines': cuisines[]-> {
+          name,
+          'id': _id,
+          'slug': slug.current
+        },
+        servings,
+        'image': image{
+          'altText': altText,
+          'imageURL': image.asset->url
+        },
+        calories
+      }
+    `;
 
-	const params = {};
+		const params = {};
 
-	const recipeData = await sanity.fetch(query, params);
+		const recipeData = await sanity.fetch(query, params);
 
-	return recipeData;
+		return recipeData;
+	} catch (error) {
+		console.error('An error occurred while fetching recipe data:', error);
+		// Handle the error or throw it to be handled in the calling code
+		throw error;
+	}
 }
