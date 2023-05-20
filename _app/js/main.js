493485ce3391b4ad8084afd5e7978c176f7a6be5
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-20 14:03:07 +0200

    Imported and created header module

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,9 +1,9 @@
 import FetchRecipeData from "./modules/fetch-recipe-data.js";
 import RenderRecipes from "./modules/recipes-list.js";
 import RenderRecipeDescription from "./modules/recipe-description.js";
-// import InstructionButton from "./modules/instruction-button.js";
 import Categories from "./modules/categories.js";
 import RecipeSearch from "./modules/recipe-search.js";
+import Header from "./modules/main-header.js";
 
 const recipeData = await FetchRecipeData();
 
@@ -11,7 +11,6 @@ RenderRecipes(recipeData);
 RenderRecipeDescription(recipeData);
 Categories(recipeData);
 RecipeSearch(recipeData);
-// InstructionButton();
+Header();
 
-// console.log(recipeData);
 
