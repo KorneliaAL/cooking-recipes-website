f5a18d035e80ba4e86410957f950332a859ec0a9
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-20 15:52:28 +0200

    Commented code

--- _app/js/modules/main-header.js
+++ _app/js/modules/main-header.js
@@ -4,16 +4,25 @@ export default function Header() {
 	const navigationList = document.querySelector('.main-header__menu');
 	navigationButton.addEventListener('click', handleNavigationButtonClick);
 
+	/**
+	 * Event handler for navigation button click.
+	 * Toggles the collapsed state and renders HTML accordingly.
+	 */
+
 	function handleNavigationButtonClick(event) {
 		isCollapsed = !isCollapsed;
-		renderHTML()
+		renderHTML();
 	}
 
+	/**
+	 * Renders the HTML based on the collapsed state.
+	 */
+	
 	function renderHTML() {
 		if (isCollapsed) {
 			navigationList.classList.add('main-header__menu--visible');
 			// Disable scroll
-			document.body.classList.add('main-header--no-scroll')
+			document.body.classList.add('main-header--no-scroll');
 		} else {
 			navigationList.classList.remove('main-header__menu--visible');
 			// Enable scroll
