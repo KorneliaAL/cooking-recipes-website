6a12ef29812fb103785f9bb8afa7c7c364f03af9
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-20 14:13:48 +0200

    Created the logic for mobile navigation

--- _app/js/modules/main-header.js
+++ _app/js/modules/main-header.js
@@ -1,3 +1,16 @@
 export default function Header() {
-	console.log('hello');
+	let isCollapsed = false;
+	const navigationButton = document.querySelector('.main-header__mobile-burger-button');
+	const navigationList = document.querySelector('.main-header__menu');
+	navigationButton.addEventListener('click', handleNavigationButtonClick);
+
+	function handleNavigationButtonClick(event) {
+		isCollapsed = !isCollapsed;
+
+		if (isCollapsed) {
+			navigationList.classList.add('main-header__menu--visible')
+		} else {
+			navigationList.classList.remove('main-header__menu--visible');
+		}
+	}
 }
