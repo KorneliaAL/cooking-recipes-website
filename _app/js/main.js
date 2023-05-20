696c71539f87905ea68c675f54dfffc9c93d9771
Kornelia Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-20 13:28:37 +0200

    Created function that filters recipes based on search input
    * Created search function
    
    * Added debounce
    
    * Reorganized the code
    
    * Commented out Instruction button module
    
    * Commented the code using JSDoc
    
    * Added @todo
    
    * Updated todo

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,15 +1,17 @@
 import FetchRecipeData from "./modules/fetch-recipe-data.js";
 import RenderRecipes from "./modules/recipes-list.js";
 import RenderRecipeDescription from "./modules/recipe-description.js";
-import InstructionButton from "./modules/instruction-button.js";
+// import InstructionButton from "./modules/instruction-button.js";
 import Categories from "./modules/categories.js";
+import RecipeSearch from "./modules/recipe-search.js";
 
 const recipeData = await FetchRecipeData();
 
 RenderRecipes(recipeData);
 RenderRecipeDescription(recipeData);
 Categories(recipeData);
-InstructionButton();
+RecipeSearch(recipeData);
+// InstructionButton();
 
 // console.log(recipeData);
 
