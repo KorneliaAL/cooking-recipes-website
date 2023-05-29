74a8af9d4788c15130059134b3dfa0530f7cab28
Kornelia Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-29 14:48:33 +0200

    Fixed bugs on header, updated styling
    * Added icons for navigation links for mobile
    
    * Added exit icon for menu button
    
    * Displayed icons to none for desktop
    
    * Added aria-label for navigation menu
    
    * Changed variable name
    
    * Added invisible text for burger menu button
    
    * Added text content for menu button
    
    * Created try..catch statement
    
    * Created temporary try/catch
    
    * Created new error messages
    
    * Created try/catch function
    
    * Added container for error message
    
    * Created README.md
    
    * Added mvp
    
    * Updated features
    
    * Created function that filters recipes based on search input
    
    * Created search function
    
    * Added debounce
    
    * Reorganized the code
    
    * Commented out Instruction button module
    
    * Commented the code using JSDoc
    
    * Added @todo
    
    * Updated todo
    
    * Created navigation for mobile
    
    * Added if statement for search container
    
    * Imported and created header module
    
    * Created the logic for mobile navigation
    
    * Created no scroll class
    
    * Added no scroll function on navigation
    
    * Commented code
    
    * Removed search button from header
    
    * Removed search button from navigation
    
    * Styled navigation for mobile version
    
    * Updated demo link
    
    * Feature: recipe roulette (#23)
    
    * Created markup for recipe roulette
    
    * Created modules
    
    * Removed todo
    
    * Rendered random recipe to recipe roulette
    
    * Created blur modifier
    
    * Imported module
    
    * Added classes
    
    * Changed variable name
    
    * Added grid column start for desktop
    
    * Added background color for recipe card
    
    * Added grid classes for mobile
    
    * Added recipe roulette section
    
    * Styled recipe roulette section
    
    * Added blur effect for safari
    
    * Fixed so the function work for search button and enter key
    
    * Added class to search button
    
    * Added event listener to enter key & search button
    
    * Fixed styling
    
    * Changed class name
    
    * Added hover effect for active favorite button
    
    * Created module for favorite button
    
    * Imported module
    
    * Removed styling for favorite button in modules
    
    * Changed the class name to the updated name
    
    * Changed file name
    
    * Changed file name
    
    * Changed file name
    
    * Changed to flex space between
    
    * Created module for search input
    
    * Changed file name
    
    * Added margin
    
    * Styled search section
    
    * Changed stroke color for search icon
    
    * Added class to input and removed type from button
    
    * Removed unnecessary styling
    
    * Decreased description title font size for mobile
    
    * Changed description title font size
    
    * Added margin
    
    * Misc changes to mobile navigation
    
    * Added bigger icons
    
    * Reorgenized styling for buttons
    
    * Turned off pointer event
    
    * Removed comment
    
    * Added comments
    
    * Styled error message container
    
    * Moved error message container outside of main
    
    * Added the updated header to all sites
    
    * Cleaned code

--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -1,8 +1,10 @@
 export default function Header() {
-	let isCollapsed = false;
+	let navigationVisible = false;
 	const navigationButton = document.querySelector('.main-header__mobile-burger-button');
 	const navigationList = document.querySelector('.main-header__menu');
+
 	navigationButton.addEventListener('click', handleNavigationButtonClick);
+
 
 	/**
 	 * Event handler for navigation button click.
@@ -10,7 +12,7 @@ export default function Header() {
 	 */
 
 	function handleNavigationButtonClick(event) {
-		isCollapsed = !isCollapsed;
+		navigationVisible = !navigationVisible;
 		renderHTML();
 	}
 
@@ -19,12 +21,16 @@ export default function Header() {
 	 */
 	
 	function renderHTML() {
-		if (isCollapsed) {
+		if (navigationVisible) {
 			navigationList.classList.add('main-header__menu--visible');
+			navigationButton.style.backgroundImage = 'url(/_app/assets/icons/exit.svg)';
+			navigationButton.textContent = 'Close Navigation Menu';
 			// Disable scroll
 			document.body.classList.add('main-header--no-scroll');
 		} else {
 			navigationList.classList.remove('main-header__menu--visible');
+			navigationButton.style.backgroundImage = 'url(/_app/assets/icons/menu-2.svg)';
+			navigationButton.textContent = 'Open Navigation Menu';
 			// Enable scroll
 			document.body.classList.remove('main-header--no-scroll');
 		}
