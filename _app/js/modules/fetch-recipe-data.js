09b4e357189cae44808c2af237b890a69e4e5992
Kornelia Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-29 14:14:59 +0200

    Created try..catch statement
    * Created temporary try/catch
    
    * Created new error messages
    
    * Created try/catch function
    
    * Added container for error message
    
    * Created README.md
    
    * Added mvp
    
    * Updated features
    
    * Created function that filters recipes based on search input
    
    * Created search function
    
    * Added debounce
    
    * Reorganized the code
    
    * Commented out Instruction button module
    
    * Commented the code using JSDoc
    
    * Added @todo
    
    * Updated todo
    
    * Created navigation for mobile
    
    * Added if statement for search container
    
    * Imported and created header module
    
    * Created the logic for mobile navigation
    
    * Created no scroll class
    
    * Added no scroll function on navigation
    
    * Commented code
    
    * Removed search button from header
    
    * Removed search button from navigation
    
    * Styled navigation for mobile version
    
    * Updated demo link
    
    * Feature: recipe roulette (#23)
    
    * Created markup for recipe roulette
    
    * Created modules
    
    * Removed todo
    
    * Rendered random recipe to recipe roulette
    
    * Created blur modifier
    
    * Imported module
    
    * Added classes
    
    * Changed variable name
    
    * Added grid column start for desktop
    
    * Added background color for recipe card
    
    * Added grid classes for mobile
    
    * Added recipe roulette section
    
    * Styled recipe roulette section
    
    * Added blur effect for safari
    
    * Fixed so the function work for search button and enter key
    
    * Added class to search button
    
    * Added event listener to enter key & search button
    
    * Fixed styling
    
    * Changed class name
    
    * Added hover effect for active favorite button
    
    * Created module for favorite button
    
    * Imported module
    
    * Removed styling for favorite button in modules
    
    * Changed the class name to the updated name
    
    * Changed file name
    
    * Changed file name
    
    * Changed file name
    
    * Changed to flex space between
    
    * Created module for search input
    
    * Changed file name
    
    * Added margin
    
    * Styled search section
    
    * Changed stroke color for search icon
    
    * Added class to input and removed type from button
    
    * Removed unnecessary styling
    
    * Decreased description title font size for mobile
    
    * Changed description title font size
    
    * Added margin
    
    * Misc changes to mobile navigation
    
    * Added bigger icons
    
    * Reorgenized styling for buttons
    
    * Turned off pointer event
    
    * Removed comment
    
    * Added comments
    
    * Styled error message container
    
    * Moved error message container outside of main

--- _app/js/modules/fetch-recipe-data.js
+++ _app/js/modules/fetch-recipe-data.js
@@ -1,39 +1,60 @@
 import { sanity } from '../sanity.js';
 
+/**
+ * Fetches recipe data from the server.
+ * If an error occurs during the fetch, a custom error message is displayed.
+ * @returns {Promise<object>} The recipe data.
+ */
+
+
 export default async function FetchRecipeData() {
-	const query = 
-		`* [_type == 'cookingRecipes'] {
-			'slug': slug.current,
-			'id': _id,
-			'recipeName': name,
-			'ingredients': ingredientList[],
-			'instructions': instructions[],
-			'category': category-> {
-				name,
-				'categorySlug': slug.current,
-				'id': _id,
-				'image': image {
-				'imageURL': image.asset -> url,
-				'altText': altText
-				}
-			},
-			'cookTime': readyInMinutes,
-			'cuisines': cuisines[] -> {
-				name,
-				'id': _id,
-				'slug': slug.current
-			},
-			servings,
-			'image': image{
-				'altText': altText,
-				'imageURL': image.asset -> .url
-			},
-			calories
-		}`;
+		const query = `
+      *[ _type == 'cookingRecipes' ] {
+        'slug': slug.current,
+        'id': _id,
+        'recipeName': name,
+        'ingredients': ingredientList[],
+        'instructions': instructions[],
+        'category': category-> {
+          name,
+          'categorySlug': slug.current,
+          'id': _id,
+          'image': image {
+            'imageURL': image.asset->url,
+            'altText': altText
+          }
+        },
+        'cookTime': readyInMinutes,
+        'cuisines': cuisines[]-> {
+          name,
+          'id': _id,
+          'slug': slug.current
+        },
+        servings,
+        'image': image{
+          'altText': altText,
+          'imageURL': image.asset->url
+        },
+        calories
+      }
+    `;
 
-	const params = {};
+	async function getDataFromTryCatch(query) {
+		const errorMessage = document.querySelector('.error-message');
+		const mainContainer = document.querySelector('main');
+		try {
+			const recipeData = await sanity.fetch(query);
+			return recipeData;
+		}
+		// Display error message from sanity-client.js
+		catch (error) {
+			errorMessage.textContent = error.message;
+			errorMessage.classList.add('error-message--visible');
+			mainContainer.classList.add('main--hidden');
+		}
+	}
 
-	const recipeData = await sanity.fetch(query, params);
+	const fetchData = getDataFromTryCatch(query);
 
-	return recipeData;
+	return fetchData;
 }
