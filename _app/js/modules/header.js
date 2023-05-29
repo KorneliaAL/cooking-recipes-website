711b0a08e73c124bf83fec5042a439c4a513f0e0
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2023-05-29 12:13:50 +0200

    Added invisible text for burger menu button

--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -2,7 +2,9 @@ export default function Header() {
 	let navigationVisible = false;
 	const navigationButton = document.querySelector('.main-header__mobile-burger-button');
 	const navigationList = document.querySelector('.main-header__menu');
+
 	navigationButton.addEventListener('click', handleNavigationButtonClick);
+
 
 	/**
 	 * Event handler for navigation button click.
@@ -22,13 +24,13 @@ export default function Header() {
 		if (navigationVisible) {
 			navigationList.classList.add('main-header__menu--visible');
 			navigationButton.style.backgroundImage = 'url(/_app/assets/icons/exit.svg)';
-			navigationButton.setAttribute('aria-label', "Close Navigation Menu");
+			navigationButton.textContent = 'Close Navigation Menu';
 			// Disable scroll
 			document.body.classList.add('main-header--no-scroll');
 		} else {
 			navigationList.classList.remove('main-header__menu--visible');
 			navigationButton.style.backgroundImage = 'url(/_app/assets/icons/menu-2.svg)';
-			navigationButton.setAttribute('aria-label', "Open Navigation Menu");
+			navigationButton.textContent = 'Open Navigation Menu';
 			// Enable scroll
 			document.body.classList.remove('main-header--no-scroll');
 		}
